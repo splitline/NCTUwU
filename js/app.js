7e569d0168a5e52a6279eded232bc17b2659f983
黃志仁 <tbsthitw@gmail.com>
2020-08-31 19:37:59 +0800

    Add brand icon. Move constants to config.js.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,5 @@
-/**
-|--------------------------------------------------
-| Constants
-|--------------------------------------------------
-*/
-
-const TIME_MAPPING = {
-    "M": "6:00 ~ 6:50",
-    "N": "7:00 ~ 7:50",
-    "A": "8:00 ~ 8:50",
-    "B": "9:00 ~ 9:50",
-    "C": "10:10 ~ 11:00",
-    "D": "11:10 ~ 12:00",
-    "X": "12:20 ~ 13:10",
-    "E": "13:20 ~ 14:10",
-    "F": "14:20 ~ 15:10",
-    "G": "15:30 ~ 16:20",
-    "H": "16:30 ~ 17:20",
-    "Y": "17:30 ~ 18:20",
-    "I": "18:30 ~ 19:20",
-    "J": "19:30 ~ 20:20",
-    "K": "20:30 ~ 21:20",
-    "L": "21:30 ~ 22:20"
-}
-
-const YEAR = '109', SEMESTER = '1';
-
-const APP_URL = `${location.protocol}//${location.host}${location.pathname}`;
-
 let courseData = {};
 let selectedCourse = {};
-let totalCredits = () => Object.keys(selectedCourse).reduce((accu, id) => +courseData[id].credit + accu, 0);
 
 // Safari sucks.
 
@@ -56,11 +26,7 @@ function loadFromLocalStorage() {
     return JSON.parse(localStorage.getItem("selectedCourse")) || {};
 }
 
-/**
-|--------------------------------------------------
-| Init UI
-|--------------------------------------------------
-*/
+const totalCredits = () => Object.keys(selectedCourse).reduce((accu, id) => +courseData[id].credit + accu, 0);
 
 let share = false;
 if (location.search.includes("share=")) {
@@ -70,7 +36,7 @@ if (location.search.includes("share=")) {
     document.querySelector(".loading").classList.remove("is-hidden");
 }
 
-// Generate timetable.
+// Render timetable.
 Object.keys(TIME_MAPPING).forEach(period => {
     const div = document.createElement("div");
     div.textContent = `${period} / ${TIME_MAPPING[period]}`;
